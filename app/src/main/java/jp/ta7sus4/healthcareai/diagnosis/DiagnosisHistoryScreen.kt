d85b3ed1e975c46aeccbdad6bb7ebbeb2aef0160
ta7sus4 <85665552+ta7sus4@users.noreply.github.com>
2023-11-17 06:49:21 +0900

    履歴がないときの表示を追加

--- app/src/main/java/jp/ta7sus4/healthcareai/diagnosis/DiagnosisHistoryScreen.kt
+++ app/src/main/java/jp/ta7sus4/healthcareai/diagnosis/DiagnosisHistoryScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -20,11 +21,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import jp.ta7sus4.healthcareai.R
 
 @Composable
 fun DiagnosisHistoryScreen(
@@ -56,21 +60,43 @@ fun DiagnosisHistoryScreen(
             }
             Spacer(modifier = Modifier.weight(0.07f))
             Text(
-                text = "履歴",
+                text = stringResource(id = R.string.history),
                 fontSize = 24.sp,
             )
             Spacer(modifier = Modifier.weight(1f))
             Text(
-                text = "全て消去",
+                text = stringResource(id = R.string.delete_all),
                 textAlign = TextAlign.Right,
                 modifier = Modifier
                     .clickable(onClick = { viewModel.historyDeleteButtonPressed() })
             )
         }
-        LazyColumn {
-            items(viewModel.historyList) { history ->
-                key(history.id) {
-                    DiagnosisHistoryCard(history = history)
+        if (viewModel.historyList.size == 0 ) {
+            Spacer(modifier = Modifier.weight(1.1f))
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    Icons.Outlined.Info,
+                    tint = Color.Gray,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(top = 2.dp, end = 4.dp)
+                        .size(22.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.no_history),
+                    fontSize = 18.sp,
+                    color = Color.Gray,
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+        } else {
+            LazyColumn {
+                items(viewModel.historyList) { history ->
+                    key(history.id) {
+                        DiagnosisHistoryCard(history = history)
+                    }
                 }
             }
         }
