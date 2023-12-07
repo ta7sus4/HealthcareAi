1d2d793196a709ddfd3855efe4e7a6e82168f207
ta7sus4 <85665552+ta7sus4@users.noreply.github.com>
2023-12-07 15:43:45 +0900

    メイン画面にロゴを追加

--- app/src/main/java/jp/ta7sus4/healthcareai/MainScreen.kt
+++ app/src/main/java/jp/ta7sus4/healthcareai/MainScreen.kt
@@ -1,19 +1,21 @@
 package jp.ta7sus4.healthcareai
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import jp.ta7sus4.healthcareai.ui.theme.HealthcareAiTheme
 
@@ -28,9 +30,10 @@ fun MainScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(modifier = Modifier.weight(1f))
-        Text(
-            text = stringResource(R.string.app_name),
-            fontSize = 30.sp,
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = null,
+            modifier = Modifier.size(180.dp)
         )
         Spacer(modifier = Modifier.weight(1f))
         OutlinedButton(
@@ -39,7 +42,7 @@ fun MainScreen(
         ) {
             Text(text = stringResource(R.string.start_chat))
         }
-        Spacer(modifier = Modifier.weight(0.2f))
+        Spacer(modifier = Modifier.weight(0.3f))
         OutlinedButton(
             onClick = { navHostController?.navigate("diagnosis") },
             border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
