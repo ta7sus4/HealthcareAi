474d8cb1267ce244edb958e9df6ffaa0b789addd
ta7sus4 <85665552+ta7sus4@users.noreply.github.com>
2023-12-07 15:44:24 +0900

    診断側も変更に合わせる

--- app/src/main/java/jp/ta7sus4/healthcareai/diagnosis/DiagnosisStartScreen.kt
+++ app/src/main/java/jp/ta7sus4/healthcareai/diagnosis/DiagnosisStartScreen.kt
@@ -1,15 +1,20 @@
 package jp.ta7sus4.healthcareai.diagnosis
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import jp.ta7sus4.healthcareai.R
 
@@ -37,11 +42,15 @@ fun DiagnosisStartScreen(
                 Text(
                     text = stringResource(id = R.string.diagnosis),
                     fontSize = 30.sp,
+                    style = TextStyle(textDecoration = TextDecoration.Underline)
                 )
                 Spacer(Modifier.weight(0.2f))
                 Text(text = stringResource(id = R.string.diagnosis_description))
                 Spacer(Modifier.weight(0.8f))
-                OutlinedButton(onClick = { viewModel.startButtonPressed() }) {
+                OutlinedButton(
+                    onClick = { viewModel.startButtonPressed() },
+                    border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+                ) {
                     Text(
                         text = if (viewModel.diagnosisState == DiagnosisState.LOADING) stringResource(id = R.string.thinking_question) else stringResource(id = R.string.start_diagnosis)
                     )
@@ -49,13 +58,14 @@ fun DiagnosisStartScreen(
                 Spacer(Modifier.weight(0.2f))
                 OutlinedButton(
                     onClick = { viewModel.historyButtonPressed() },
-                    enabled = viewModel.diagnosisState != DiagnosisState.LOADING
+                    enabled = viewModel.diagnosisState != DiagnosisState.LOADING,
+                    border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
                 ) {
                     Text(
                         text = stringResource(id = R.string.show_history)
                     )
                 }
-                Spacer(Modifier.weight(1f))
+                Spacer(Modifier.weight(0.6f))
             }
         }
     }
